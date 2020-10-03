bfd6e1742b9801615b8c27a66355614322cf0464
sepfy <sepfy95@gmail.com>
2020-10-03 12:52:27 +0100

    Access gpio by sysfs

--- 02-gpio-interrupt/button.c
+++ 02-gpio-interrupt/button.c
@@ -14,6 +14,32 @@ static unsigned long flags = 0;
 int buzzer_trigger = 0;
 u32 trigger_jiffies;
 
+static ssize_t attr_store(struct device *dev,
+		struct device_attribute *attr,
+		const char *buf, size_t count)
+{
+
+	printk("[%s][%d]\n", __func__, __LINE__);
+	buzzer_trigger = simple_strtoul(buf, NULL, 10);
+	gpio_set_value(BUZZER, buzzer_trigger);
+	return count;
+}
+
+static ssize_t attr_show(struct device *dev,
+		struct device_attribute *attr,
+		char *buf)
+{
+        int val = 0;
+	printk("[%s][%d]\n", __func__, __LINE__);
+        val = sprintf(buf, "%d\n", buzzer_trigger);
+        return val;
+}
+
+static struct class *mydev_class;
+static struct device *dev;
+
+static DEVICE_ATTR(data, 0644, attr_show, attr_store);
+
 static irqreturn_t button_isr(int irq, void *data)
 {
 	local_irq_save(flags);
@@ -22,7 +48,7 @@ static irqreturn_t button_isr(int irq, void *data)
 	{
 		trigger_jiffies = jiffies;
 		gpio_set_value(BUZZER, buzzer_trigger);
-		buzzer_trigger = buzzer_trigger ? (0):(1);
+		buzzer_trigger = buzzer_trigger ? 0 : 1;
 	}  
 	local_irq_restore(flags);
 	return IRQ_HANDLED;
@@ -31,6 +57,27 @@ static irqreturn_t button_isr(int irq, void *data)
 
 int init_module(void)
 {
+	int ret = 0;
+	printk("Init my dev\n");
+	mydev_class = class_create(THIS_MODULE, "mydev_class");
+	if(IS_ERR(mydev_class)) {
+		ret = PTR_ERR(mydev_class);
+		printk(KERN_ALERT "Failed to create class.\n");
+		return ret;
+	}
+
+	dev = device_create(mydev_class, NULL, MKDEV(100,0), NULL, "dev");
+	if(IS_ERR(dev)) {
+		ret = PTR_ERR(dev);
+		printk(KERN_ALERT "Failed to create device.\n");
+		return ret;
+	}
+
+	ret = device_create_file(dev, &dev_attr_data);
+	if(ret < 0) {
+		printk(KERN_ALERT "Failed to create attribute file.\n");
+		return ret;
+	}
 
 	printk("[%s][%d]\n", __func__, __LINE__);
 	trigger_jiffies = jiffies;
@@ -44,7 +91,7 @@ int init_module(void)
 
 	if(!gpio_is_valid(BUTTON))
 		return -1;
-	if(gpio_request(BUTTON,"BUTTON") < 0)
+	if(gpio_request(BUTTON, "BUTTON") < 0)
 		return -1;
 
 	button_irq = gpio_to_irq(BUTTON);
@@ -57,6 +104,11 @@ int init_module(void)
 
 void cleanup_module(void)
 {
+	printk("[%s][%d]\n", __func__, __LINE__);
+	device_remove_file(dev, &dev_attr_data);
+	device_destroy(mydev_class, MKDEV(100, 0));
+	class_destroy(mydev_class);
+
 	gpio_set_value(BUZZER, 0);
 	gpio_free(BUZZER);
 	free_irq(button_irq, NULL);
